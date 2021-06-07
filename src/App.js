a1c7498d1244a27c654e65885ff7d4d36c6517dd
mmestiyak <mmestiyak.contact@gmail.com>
2021-06-07 16:55:26 +0600

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
