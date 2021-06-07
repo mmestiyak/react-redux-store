f27ca6418cd636ec24c797c228fcf51a935e9109
mmestiyak <mmestiyak.contact@gmail.com>
2021-06-07 22:24:01 +0600

    added not-found message  on invalid page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ProductListing from "./components/ProductListing";
-import ProductDetails from "./components/ProductDetails";
-import Header from './components/Header'
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import ProductListing from './components/ProductListing';
+import ProductDetails from './components/ProductDetails';
+import Header from './components/Header';
 function App() {
   return (
-   <>
+    <>
       <Router>
         <Header />
         <Switch>
           <Route path="/" exact component={ProductListing} />
-          <Route path="/product/:productId" component={ProductDetails} />
-          <Route>404 Not Found!</Route>
+          <Route exact path="/product/:productId" component={ProductDetails} />
+          <Route path="*">
+            <h2 style={{ marginTop: '10rem', textAlign: 'center' }}>
+              Nothing Found, Go Back <Link to="/">Home Page</Link>
+            </h2>
+          </Route>
         </Switch>
       </Router>
-   </>
+    </>
   );
 }
 
