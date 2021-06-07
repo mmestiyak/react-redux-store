ee572875416f29123498e7471dca1705cc91cd54
mmestiyak <mmestiyak.contact@gmail.com>
2021-06-07 22:07:36 +0600

    responsive issues fixed

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-      <div className="four wide column" key={id}>
+      <div className="column" key={id}>
         <Link to={`/product/${id}`}>
           <div className="ui link cards">
             <div className="card">
--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useCallback, useMemo } from "react";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { setProducts } from "../redux/actions/productsActions";
-import ProductComponent from "./ProductComponent";
+import React, { useEffect, useCallback, useMemo } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from '../redux/actions/productsActions';
+import ProductComponent from './ProductComponent';
 
 const ProductPage = () => {
   const products = useSelector((state) => state.allProducts.products);
   const dispatch = useDispatch();
   const fetchProducts = async () => {
     const response = await axios
-      .get("https://fakestoreapi.com/products")
+      .get('https://fakestoreapi.com/products')
       .catch((err) => {
-        console.log("Err: ", err);
+        console.log('Err: ', err);
       });
     dispatch(setProducts(response.data));
   };
@@ -20,10 +20,12 @@ const ProductPage = () => {
     fetchProducts();
   }, []);
 
-  console.log("Products :", products);
+  console.log('Products :', products);
   return (
     <div className="ui grid container">
-      <ProductComponent />
+      <div class="ui stackable four column grid">
+        <ProductComponent />
+      </div>
     </div>
   );
 };
