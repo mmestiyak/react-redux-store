20ebb4551f052af207b81668f62b01a2f46e7312
Asraful Islam <iasraful321@gmail.com>
2021-06-07 19:59:38 +0600

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello</h1>
-      </header>
-    </div>
+   <>
+    <Header/>
+   </>
   );
 }
 
